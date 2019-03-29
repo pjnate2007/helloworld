f87d1cd9cf7cf1b6874e8c465cda0e2b10fe3ecc
Nathan Glass <pjnate2007@gmail.com>
2019-03-29 18:11:18 -0500

    showing off for nate

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var i = 0;
+            Console.WriteLine("Hello World! " + i);
         }
     }
 }
